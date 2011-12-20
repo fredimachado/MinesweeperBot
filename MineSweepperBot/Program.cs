edd96e5b939cd65379a2dba424442999b9133368
Fredi <fredisoft@gmail.com>
2011-12-20 11:22:57 -0200

    Initial commit

---
+++ MineSweepperBot/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace MineSweepperBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
